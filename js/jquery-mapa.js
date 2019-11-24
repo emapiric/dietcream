1118809fad24457a9a862da777cf6485f899a37e
emapiric <57675835+emapiric@users.noreply.github.com>
2019-11-24 18:18:37 +0100

    fix map marker

--- js/jquery-mapa.js
+++ js/jquery-mapa.js
@@ -1,9 +1,6 @@
 $(function() {
   $("#map").googleMap();
   $("#map").addMarker({
-    coords: [44.816544, 20.458509],
-    url:
-      "https://maps.google.com/maps?q=Knez%20mihailova%2015&t=&z=13&ie=UTF8&iwloc=&output=embed",
-    id: "pozicija"
+    coords: [44.816544, 20.458509]
   });
 });
